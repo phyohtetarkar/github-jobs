e11c2cc9976dc6ea7e18862646ed771ccd814797
Phyo Htet Arkar <phyohtetarkar@gmail.com>
2018-07-29 10:59:46 +0630

    iOS first commit

---
+++ github-jobs-ios/github-jobs-ios/JobPositionsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  github-jobs-ios
+//
+//  Created by Phyo Htet Arkar on 7/29/18.
+//  Copyright © 2018 Phyo Htet Arkar. All rights reserved.
+//
+
+import UIKit
+
+class JobPositionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
